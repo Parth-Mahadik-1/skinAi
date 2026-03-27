f297707c75d67cb38297212e92d9a8d7e808ef18
MR.Parth mahadik <parthmahadik44@gmail.com>
2026-03-27 08:57:00 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(
     __name__,
-    template_folder=os.path.join(BASE_DIR, "../git branch -M mainfrontend/templates"),
+    template_folder=os.path.join(BASE_DIR, "..frontend/templates"),
     static_folder=os.path.join(BASE_DIR, "../frontend/static")
 )
 
